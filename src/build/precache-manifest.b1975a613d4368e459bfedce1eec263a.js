8a7829cf2faee0646024b5e9bba37fbc2110f6c6
Wenhao Shu <wshu7@student.monash.edu>
2020-08-23 18:23:40 +1000

    First commit

---
+++ src/build/precache-manifest.b1975a613d4368e459bfedce1eec263a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c38b87329f2a9da20d83340dbc9e0a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6edf1c44c57d5d3b6a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e4322257ad2219a5c7c5",
+    "url": "/static/js/2.f059a9ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f059a9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6edf1c44c57d5d3b6a",
+    "url": "/static/js/main.81c437f3.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
